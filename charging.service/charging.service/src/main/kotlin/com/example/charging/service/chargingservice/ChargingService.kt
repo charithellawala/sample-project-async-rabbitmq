26be0987e7d2e086ba96c044ff9a7f4f7c7d78da
charith ellawala <charithpellawala@gmail.com>
2025-10-09 15:22:45 +0200

    some changes before claud executions

--- charging.service/charging.service/src/main/kotlin/com/example/charging/service/chargingservice/ChargingService.kt
+++ charging.service/charging.service/src/main/kotlin/com/example/charging/service/chargingservice/ChargingService.kt
@@ -4,5 +4,10 @@ import com.example.charging.service.dto.ChargingRequestDto
 import com.example.charging.service.model.StartApiResponse
 
 interface ChargingService {
+
+    companion object {
+        const val AUTH_EXCHANGE = "auth.exchange"
+        const val AUTH_REQUEST_ROUTING_KEY = "auth.request"
+    }
     fun processChargingRequest(request: ChargingRequestDto) : StartApiResponse
 }
--- charging.service/charging.service/src/main/kotlin/com/example/charging/service/chargingservice/serviceImpl/ChargingServiceImpl.kt
+++ charging.service/charging.service/src/main/kotlin/com/example/charging/service/chargingservice/serviceImpl/ChargingServiceImpl.kt
@@ -1,21 +1,23 @@
 package com.example.charging.service.chargingservice.serviceImpl
 
 import com.example.charging.service.chargingservice.ChargingService
+import com.example.charging.service.chargingservice.ChargingService.Companion.AUTH_EXCHANGE
+import com.example.charging.service.chargingservice.ChargingService.Companion.AUTH_REQUEST_ROUTING_KEY
 import com.example.charging.service.config.TimeOutConfigService
 import com.example.charging.service.dto.AuthRequestDto
 import com.example.charging.service.dto.ChargingRequestDto
 import com.example.charging.service.model.ChargingRequestEntity
 import com.example.charging.service.model.StartApiResponse
+import com.example.charging.service.rabbitmq.RabbitMqService
 import com.example.charging.service.repository.ChargingRequestRepository
 import org.slf4j.LoggerFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Service
 import java.time.Instant
 
 @Service
 class ChargingServiceImpl(
     private val chargingRequestRepository: ChargingRequestRepository,
-    private val rabbitTemplate: RabbitTemplate,
+    private val rabbitMqService: RabbitMqService,
     private val timeoutHandler: TimeOutConfigService
 ) : ChargingService {
 
@@ -42,7 +44,7 @@ class ChargingServiceImpl(
                 timestamp = Instant.now().toString()
             )
             logger.info("Sending Charging Request to Rabbitmq template")
-            rabbitTemplate.convertAndSend("auth.exchange", "auth.request", message)
+            rabbitMqService.sendAuthRequestAsTemplate(AUTH_EXCHANGE, AUTH_REQUEST_ROUTING_KEY, message)
 
             // Delegate timeout scheduling
             timeoutHandler.scheduleTimeout(requestEntity.id!!)
